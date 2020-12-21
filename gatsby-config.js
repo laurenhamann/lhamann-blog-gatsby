075cbdde2c186b54458a1009431d5ba38cec785a
lauren <waller.lauren.lw@gmail.com>
2020-12-20 19:39:14 -0500

    site-metadata and gatsby-plugin(mdx)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  siteMetadata: {
+    title: "Lauren's Blog",
+    description: "Portfolio and Blog to detail my journey in web development"
+  },
   plugins: [
     "gatsby-plugin-emotion",
     "gatsby-plugin-image",
@@ -6,6 +10,15 @@ module.exports = {
     "gatsby-plugin-mdx",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js')
+        },
+        gatsbyRemarkPlugins:[{ resolve: 'gatsby-remark-images'}],
+      }
+    },
     {
       resolve: "gatsby-source-filesystem",
       options: {
@@ -21,6 +34,23 @@ module.exports = {
         path: "./src/pages/",
       },
       __key: "pages",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: 'posts',
+        path:  `${__dirname}/src/posts`,
+      },
+      __key: "posts",
+    },
+    {
+      resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+      options: {
+        production: true,
+        disable: !process.env.ANALYZE_BUNDLE_SIZE,
+        generateStatsFile: true,
+        analyzerMode: 'static',
+      },
     },
   ],
 };
