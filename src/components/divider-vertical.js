fc41b4a6fcb9f9a6f8c195916b18cbd5ec506916
lauren <waller.lauren.lw@gmail.com>
2020-12-20 19:48:10 -0500

    added styled divider

---
+++ src/components/divider-vertical.js
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+const VerticalDivider = styled('div')`
+    color: #a2aebf;
+    width: 1px;
+    height: 51px;
+    border: 1px solid #a2aebf;
+`;
+
+
+
+export default VerticalDivider;
