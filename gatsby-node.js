a64945d550002cedcb70c2678105e6fbea6e62c3
lauren <waller.lauren.lw@gmail.com>
2020-12-23 16:20:12 -0500

    Add filter method to createpages for both projects and pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,31 @@
+// exports.createPages = async({ actions, graphql, reporter }) => 
+// {
+//     const result = await graphql(`
+//         query {
+//             allMdx {
+//                 nodes {
+//                     frontmatter {
+//                         slug
+//                     }
+//                 }
+//             }
+//         }
+//     `);
+//     if (result.errors) {
+//         reporter.panic('failed to create post', result.errors);
+//     }
+//     const posts = result.data.allMdx.nodes;
+//     posts.forEach(post => {
+//         actions.createPage({
+//             path: post.frontmatter.slug,
+//             component: require.resolve('./src/templates/post.js'),
+//             context: {
+//                 slug: post.frontmatter.slug
+//             },
+//         });
+//     })
+// }
+
 exports.createPages = async({ actions, graphql, reporter }) => 
 {
     const result = await graphql(`
@@ -6,6 +34,7 @@ exports.createPages = async({ actions, graphql, reporter }) =>
                 nodes {
                     frontmatter {
                         slug
+                        type
                     }
                 }
             }
@@ -14,13 +43,27 @@ exports.createPages = async({ actions, graphql, reporter }) =>
     if (result.errors) {
         reporter.panic('failed to create post', result.errors);
     }
-    const posts = result.data.allMdx.nodes;
+    const entries = result.data.allMdx.nodes;
+
+    const posts = entries.filter(entry => entry.frontmatter.type === "post");
     posts.forEach(post => {
         actions.createPage({
             path: post.frontmatter.slug,
             component: require.resolve('./src/templates/post.js'),
             context: {
                 slug: post.frontmatter.slug
+            },
+        });
+    })
+
+    const projects = entries.filter(entry => entry.frontmatter.type === "project");
+
+    projects.forEach(project => {
+        actions.createPage({
+            path: project.frontmatter.slug,
+            component: require.resolve('./src/templates/project.js'),
+            context: {
+                slug: project.frontmatter.slug
             },
         });
     })
