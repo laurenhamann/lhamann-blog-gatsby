0d72ff61de9609f0d25596e2cfc92b22f8680a47
Lauren <waller.lauren.lw@gmail.com>
2021-01-03 16:58:17 -0500

    Change Image size

--- src/hooks/use-posts.js
+++ src/hooks/use-posts.js
@@ -11,7 +11,7 @@ const usePosts = () => {
                 title
                 image {
                     sharp: childImageSharp {
-                    fluid(maxHeight: 100, maxWidth: 100) {
+                    fluid(maxHeight: 350, maxWidth: 400) {
                         ...GatsbyImageSharpFluid_withWebp
                     }
                     }
